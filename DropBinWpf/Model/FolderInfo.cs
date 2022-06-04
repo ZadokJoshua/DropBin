90a9b49e08e9636ab0cd2618545d5dddc9f80b97
Zadok Joshua <ayegbazadok@outlook.com>
2022-06-04 20:14:56 +0100

    Add FolderInfo Class

---
+++ DropBinWpf/Model/FolderInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DropBinWpf.Model
+{
+    public class FolderInfo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string FolderPath { get; set; }
+        public string ZippedFilePath { get; set; }
+    }
+}
