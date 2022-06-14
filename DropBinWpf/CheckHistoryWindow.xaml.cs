333f422350a9b80a9cdecc8b27bfb850f968688c
Zadok Joshua <ayegbazadok@outlook.com>
2022-06-14 15:07:57 +0100

    Created Delete Feature

--- DropBinWpf/CheckHistoryWindow.xaml.cs
+++ DropBinWpf/CheckHistoryWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DropBinWpf.DataAccess;
+using DropBinWpf.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +24,35 @@ namespace DropBinWpf
         public CheckHistoryWindow()
         {
             InitializeComponent();
+            Load();
+        }
+
+        public void Load()
+        {
+            using (var db = new FolderInfoContext())
+            {
+                myDataGrid.ItemsSource = db.FolderInfo.ToList();
+            }
+        }
+
+        private void deleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int Id = (myDataGrid.SelectedItem as FolderInfo).Id;
+                using (var db = new FolderInfoContext())
+                {
+                    var deleteMember = db.FolderInfo.Where(m => m.Id == Id).Single();
+                    db.FolderInfo.Remove(deleteMember);
+                    db.SaveChanges();
+                    myDataGrid.ItemsSource = db.FolderInfo.ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                //throw;
+            }
         }
     }
 }
